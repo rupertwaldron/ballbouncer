f3ae01a27e6890de30807c2fd4dee2b1e6ba44dd
rupertwaldron <rupert.waldron@yahoo.co.uk>
2022-01-29 08:14:45 +0000

    running ok without modules

--- src/test/java/com/ruppyrup/bigfun/ServerApplicationTest.java
+++ src/test/java/com/ruppyrup/bigfun/ServerApplicationTest.java
@@ -1,5 +1,8 @@
 package com.ruppyrup.bigfun;
 
+import static org.testfx.api.FxToolkit.registerPrimaryStage;
+
+import java.util.concurrent.TimeoutException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -7,6 +10,7 @@ import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.api.FxToolkit;
@@ -15,34 +19,46 @@ import org.testfx.framework.junit5.ApplicationTest;
 
 class ServerApplicationTest extends ApplicationTest {
 
-//    @Override
-//    public void start(Stage stage) throws Exception {
-//        Parent root = FXMLLoader.load(getClass().getResource("server.fxml"));
-//        Scene scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.setResizable(false);
-//        stage.setTitle("Echo Server");
-//        stage.show();
-//    }
-//
-//    @BeforeEach
-//    public void setUp () throws Exception {
-//
-//    }
-//
-//    @AfterEach
-//    public void tearDown () throws Exception {
-//        FxToolkit.hideStage();
-//        press(new MouseButton[]{});
-//    }
-//
-//    @Test
-//    void testReset() {
-//        clickOn("#resetButton");
-//
-//        Assertions.assertEquals(100, 100);
-//
-//    }
+    @BeforeAll
+    public static void setup() throws TimeoutException {
+        if (Boolean.getBoolean("headless")) {
+            System.setProperty("testfx.robot", "glass");
+            System.setProperty("testfx.headless", "true");
+            System.setProperty("prism.order", "sw");
+            System.setProperty("prism.text", "t2k");
+            System.setProperty("java.awt.headless", "true");
+        }
+        registerPrimaryStage();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("server.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("Echo Server");
+        stage.show();
+    }
+
+    @BeforeEach
+    public void setUp () throws Exception {
+
+    }
+
+    @AfterEach
+    public void tearDown () throws Exception {
+        FxToolkit.hideStage();
+        press(new MouseButton[]{});
+    }
+
+    @Test
+    void testReset() {
+        clickOn("#resetButton");
+
+        Assertions.assertEquals(100, 100);
+
+    }
 
 
 }
