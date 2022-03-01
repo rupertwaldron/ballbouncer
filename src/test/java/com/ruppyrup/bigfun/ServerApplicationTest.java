d2590d9b5a0045f56a3125d6c5cf020972192e00
rupertwaldron <rupert.waldron@yahoo.co.uk>
2022-03-01 06:36:37 +0000

    added headless mode

--- src/test/java/com/ruppyrup/bigfun/ServerApplicationTest.java
+++ src/test/java/com/ruppyrup/bigfun/ServerApplicationTest.java
@@ -19,18 +19,6 @@ import org.testfx.framework.junit5.ApplicationTest;
 
 class ServerApplicationTest extends ApplicationTest {
 
-    @BeforeAll
-    public static void setup() throws TimeoutException {
-        if (Boolean.getBoolean("headless")) {
-            System.setProperty("testfx.robot", "glass");
-            System.setProperty("testfx.headless", "true");
-            System.setProperty("prism.order", "sw");
-            System.setProperty("prism.text", "t2k");
-            System.setProperty("java.awt.headless", "true");
-        }
-        registerPrimaryStage();
-    }
-
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("server.fxml"));
